12ef792798566f9b34c126355a8030c89d9bde4f
Majid Razvi <majidrazvi@gmail.com>
2019-12-14 02:27:12 -0700

    move all drag-related styles into useDrag

--- src/components/Bauble.js
+++ src/components/Bauble.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { animated } from 'react-spring';
-import { IMAGES, COLORS } from '../content';
+import { IMAGES } from '../content';
 import useDrag from '../hooks/useDrag'
 
 export default ({ bauble, baubleState }) => {
-  const [ activeBauble, setActiveBauble ] = baubleState;
+  const [ , setActiveBauble ] = baubleState;
   const { dragStyles, bindDragInteraction } = useDrag(bauble, setActiveBauble);
   const { imgSrc } = IMAGES[bauble];
-  const { backgroundColor } = COLORS[bauble];
-  const isActiveBauble = bauble === activeBauble;
 
   return (
     <animated.img
       className='bauble'
       src={imgSrc}
-      style={{
-        border: isActiveBauble ? `6px solid ${backgroundColor}` : 'transparent',
-        cursor: isActiveBauble ? 'grabbing' : 'grab',
-        ...dragStyles
-      }}
+      style={{ ...dragStyles }}
       {...bindDragInteraction()}
     />
   )
--- src/hooks/useDrag.js
+++ src/hooks/useDrag.js
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import { useSpring } from 'react-spring';
 import { useGesture } from 'react-with-gesture';
 
 export default (bauble, setActiveBauble) => {
+  const [isGrabbed, setIsGrabbed] = useState(false)
   const [{ xy }, setXY] = useSpring(() => ({ xy: [0, 0] }))
   const bindDragInteraction = useGesture(({ event, down, delta }) => {
     event.preventDefault()
     setXY({ xy: down ? delta : [0,0] })
     setActiveBauble(bauble)
+    setIsGrabbed(down)
   })
   return {
     dragStyles: { 
+      cursor: isGrabbed ? 'grabbing' : 'grab',
+      zIndex: isGrabbed ? 1 : 0,
       transform: xy.interpolate((x, y) => `translate(${x}px,${y}px)`),
     },
     bindDragInteraction
