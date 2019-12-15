589d71efba4fcd520a2a7d89e6ea340b69dad981
Majid Razvi <mrazvi@pivotal.io>
2019-12-15 04:29:27 -0700

    rename some vars

--- src/hooks/useBounces.js
+++ src/hooks/useBounces.js
@@ -1,11 +1,11 @@
 import { useSprings } from 'react-spring';
 
-export default n => useSprings(
-  n,
-  i => ({
+export default count => useSprings(
+  count,
+  n => ({
     from: { transform: 'translate(0, -10vh)' },
     to: { transform: 'translate(0, 0vh)' },
     config: { mass: 1, tension: 120, friction: 6, velocity: 5 },
-    delay: () => i * 70
+    delay: () => n * 70
   })
 )
