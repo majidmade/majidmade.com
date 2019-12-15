3d9648fcae829a2e8058b005ac63a42162b01ee6
Majid Razvi <mrazvi@pivotal.io>
2019-12-14 20:43:27 -0700

    reformat useDrag

--- src/hooks/useFadeIn.js
+++ src/hooks/useFadeIn.js
@@ -2,7 +2,10 @@ import { useTrail } from 'react-spring';
 import content from '../content';
 
 export default (activeContent) => {
-  const [intro, setIntro] = useTrail(content.length, () => ({ opacity: 0, config: { duration: 1500 } }))
+  const [intro, setIntro] = useTrail(
+    content.length,
+    () => ({ opacity: 0, config: { duration: 1500 }})
+  )
   if (!activeContent) {
     setIntro({ opacity: 1 })
   }
