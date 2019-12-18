0bb74058f0c9e7366898492e7efd3b2abcb75d76
Majid Razvi <mrazvi@pivotal.io>
2019-12-17 19:32:31 -0700

    redirect to https

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const p = window.location.protocol;
+if (p === 'http:' && document.domain !== 'localhost') {
+  window.location.protocol = 'https:'
+}
+
 window.oncontextmenu = e => {
   // so mobile doesnt try to download the images on long-press
   e.preventDefault()
