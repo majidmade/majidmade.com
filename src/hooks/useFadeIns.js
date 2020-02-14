622c4d2ddc88a3009c5f6e4a2982d324493e15e0
Majid Razvi <mrazvi@pivotal.io>
2020-02-13 20:40:01 -0700

    rm unused fadeOut hook

--- src/hooks/useFadeIns.js
+++ src/hooks/useFadeIns.js
@@ -1,16 +1,12 @@
 import { useSprings } from 'react-spring';
 
-const useFades = ({ reverse, count, delay }) => useSprings(
+export const useFadeIns = (count, delay = 70) => useSprings(
   count,
   i => ({ 
     from: { opacity: 0 },
     to: { opacity: 1 },
-    reverse,
     config: { duration: 1500 },
     delay: () => i * delay,
     reset: true
   })
 )
-
-export const useFadeIns = (count, delay = 70) => useFades({ reverse: false, count, delay })
-export const useFadeOuts = (count, delay = 70) => useFades({ reverse: true, count, delay })
