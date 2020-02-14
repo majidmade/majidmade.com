a61aa1afaba7745ad728884afdf0fbb462a02c1a
Majid Razvi <mrazvi@pivotal.io>
2020-02-13 19:23:40 -0700

    add placeholder descriptions (not rendered)

--- src/components/ActiveContent.js
+++ src/components/ActiveContent.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import { animated } from 'react-spring';
 import useColor from '../hooks/useColor';
-import { ContentContext, TITLES } from '../content';
+import { ContentContext, TITLES, DESCRIPTIONS } from '../content';
 
 export default () => {
   const activeContent = useContext(ContentContext);
-  const { title, subtitle } = TITLES[activeContent] || { title: '', subtitle: '' }
+  const { title, subtitle } = TITLES[activeContent];
+  const { description } = DESCRIPTIONS[activeContent];
   const { textColor } = useColor(activeContent);
   return (
     <animated.div className="active-content" style={{ color: textColor }}>
       <h1>{title}</h1>
       <h3>{subtitle}</h3>
+      {/* <p>{description}</p> */}
     </animated.div>
   )
 }
--- src/content/index.js
+++ src/content/index.js
@@ -42,3 +42,12 @@ export const TITLES = new Proxy({
   [VMWARE]: { title: 'VMware', subtitle: '☁️ Software Engineer' },
   [FALLBACK]: { title: '', subtitle: '' },
 }, withFallback);
+
+export const DESCRIPTIONS = new Proxy({
+  [AVATAR]: { description: `Lorem ipsum dolor amet hammock actually waistcoat lomo taxidermy raclette, chillwave sartorial leggings drinking vinegar. Master cleanse fashion axe coloring book authentic.` },
+  [VCU]: { description: `Pork belly fashion axe pitchfork, etsy hot chicken marfa yr. 3 wolf moon asymmetrical neutra unicorn tote bag kale chips. Taxidermy before they sold out cliche, synth kinfolk irony brunch williamsburg PBR&B authentic aesthetic pop-up deep v cloud bread.` },
+  [SAGE]: { description: `Tacos migas meh hella typewriter photo booth franzen. Letterpress vape roof party, cred irony kickstarter pour-over direct trade 90's tumeric photo.` },
+  [PIVOTAL]: { description: `Kogi next level chia edison bulb enamel pin prism taiyaki marfa. YOLO polaroid hot chicken, williamsburg direct trade vape four loko. Butcher chia pabst, tacos four dollar toast.` },
+  [VMWARE]: { description: `Seitan hexagon tbh,` },
+  [FALLBACK]: { description: '' },
+}, withFallback) 
