5aee59de2d7c4fa6673f0488285dbec6b64711ac
Majid Razvi <mrazvi@pivotal.io>
2020-02-13 17:17:51 -0700

    add fallback mechanism to content exports

--- src/content/index.js
+++ src/content/index.js
@@ -2,26 +2,35 @@ const AVATAR = Symbol('AVATAR');
 const VCU = Symbol('VCU');
 const SAGE = Symbol('SAGE');
 const PIVOTAL = Symbol('PIVOTAL');
+const FALLBACK = Symbol('DONTEXPORTME');
 
 export default [ AVATAR, VCU, SAGE, PIVOTAL ]
 
-export const IMAGES = {
+// dev QoL
+const withFallback = {
+  get: (content, symbol) => content[symbol] || content[FALLBACK] || {}
+}
+
+export const IMAGES = new Proxy({
   [AVATAR]: { imgSrc: '/avatar.jpg' },
   [VCU]: { imgSrc: '/vcu.png' },
   [SAGE]: { imgSrc: '/sage.png' },
   [PIVOTAL]: { imgSrc: '/pivotal.png' },
-};
+  [FALLBACK]: { imgSrc: '' },
+}, withFallback);
 
-export const COLORS = {
+export const COLORS = new Proxy({
   [AVATAR]: { backgroundColor: 'rgb(130, 49, 35)', textColor: 'white', glowColor: 'rgb(20, 12, 14)' },
   [VCU]: { backgroundColor: 'rgb(252, 171, 18)', textColor: 'black', glowColor: 'black' },
   [SAGE]: { backgroundColor: 'rgb(3, 38, 56)', textColor: 'white', glowColor: 'rgb(29, 218, 9)' },
   [PIVOTAL]: { backgroundColor: '#b2f1e8', textColor: 'black', glowColor: 'rgb(22, 169, 144)' },
-}
+  [FALLBACK]: { backgroundColor: 'white', textColor: 'black', glowColor: 'black' },
+}, withFallback);
 
-export const TITLES = {
+export const TITLES = new Proxy({
   [AVATAR]: { title: 'Majid Razvi', subtitle: '❤️ Denver, CO' },
   [VCU]: { title: 'VCU', subtitle: '🎓 Philosophy' },
   [SAGE]: { title: 'Sage', subtitle: '💸 Software Engineer' },
   [PIVOTAL]: { title: 'Pivotal', subtitle: '☁️ Software Engineer' },
-}
+  [FALLBACK]: { title: '', subtitle: '' },
+}, withFallback);
