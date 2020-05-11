42525d20c5f1f65131c5ad5cf3202c12fa312ab9
Majid Razvi <mrazvi@pivotal.io>
2020-05-11 10:29:11 -0600

    disable https redirect while migrating hosts and domains and stuff

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const p = window.location.protocol;
-const d = document.domain;
-if (p === 'http:'
-&& !(d.startsWith('localhost') || d.startsWith('10'))) {
-  window.location.protocol = 'https:'
-}
+// const p = window.location.protocol;
+// const d = document.domain;
+// if (p === 'http:'
+// && !(d.startsWith('localhost') || d.startsWith('10'))) {
+//   window.location.protocol = 'https:'
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
